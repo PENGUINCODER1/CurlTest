a49e43f12ad6216e9dc234b29cfd6457a9774d9b
CoderPenguin1 <76060071+PENGUINCODER1@users.noreply.github.com>
2023-12-05 16:54:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+from os import scandir, system
+from time import sleep
+
+frame = 1
+max_frame = 10
+while True:
+  if frame == max_frame + 1:
+    frame = 1
+  f = open(f"frames/{frame}")
+  print(f.read())
+  frame = frame + 1
+  sleep(0.1)
+  system("clear")
